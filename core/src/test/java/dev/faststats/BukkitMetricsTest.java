48067b8a4be3fc9d5e194ffcc6761dcec1f6eb1e
david <mail@nonswag.dev>
2026-01-02 19:43:38 +0100

    Refactor submitAsync with exception handling

--- core/src/test/java/dev/faststats/BukkitMetricsTest.java
+++ core/src/test/java/dev/faststats/BukkitMetricsTest.java
@@ -2,25 +2,15 @@ package dev.faststats;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import java.util.zip.GZIPOutputStream;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class BukkitMetricsTest {
     @Test
     public void testCreateData() throws IOException {
-        var mock = new MockMetrics(UUID.randomUUID(), "bba4a14eac38779007a6fda4814381ab", true);
-        var data = mock.createData();
-        var bytes = data.toString().getBytes(StandardCharsets.UTF_8);
-        try (var byteOutput = new ByteArrayOutputStream();
-             var output = new GZIPOutputStream(byteOutput)) {
-            output.write(bytes);
-            output.finish();
-            var compressed = byteOutput.toByteArray();
-            mock.printInfo(new String(compressed, StandardCharsets.UTF_8) + " (" + compressed.length + " bytes)");
-            mock.printInfo(new String(bytes, StandardCharsets.UTF_8) + " (" + bytes.length + " bytes)");
-        }
+        var mock = new MockMetrics(UUID.randomUUID(), "24f9fc423ed06194065a42d00995c600", true);
+        assumeTrue(mock.submitAsync().join(), "For this test to run, the server must be running");
     }
 }
