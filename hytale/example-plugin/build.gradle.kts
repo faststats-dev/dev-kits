d7d3c9c8e163b4dddef26cc1fbdf858433e7a891
david <mail@nonswag.dev>
2026-01-17 19:34:09 +0100

    Add server download automation

--- hytale/example-plugin/build.gradle.kts
+++ hytale/example-plugin/build.gradle.kts
@@ -2,8 +2,11 @@ plugins {
     id("com.gradleup.shadow") version "9.3.1"
 }
 
+val libsDir: Directory = project(":hytale").layout.projectDirectory.dir("libs")
+val hytaleServerJar: RegularFile = libsDir.file("HytaleServer.jar")
+
 dependencies {
-    compileOnly(files("../HytaleServer.jar"))
+    compileOnly(files(hytaleServerJar))
     implementation(project(":hytale"))
 }
 
