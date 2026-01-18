b16c432a0db55ae8dd9284603849711075b80ddc
david <mail@nonswag.dev>
2026-01-18 18:36:12 +0100

    Remove impl package from module exports

--- error-tracker/src/main/java/module-info.java
+++ error-tracker/src/main/java/module-info.java
@@ -4,7 +4,6 @@ import org.jspecify.annotations.NullMarked;
 module dev.faststats.errors {
     exports dev.faststats.errors.concurrent;
     exports dev.faststats.errors;
-    exports dev.faststats.errors.impl;
 
     requires com.google.gson;
     
