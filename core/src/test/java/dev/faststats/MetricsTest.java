ed61c87a6de2466a56147e47c4611baf6fa32aae
david <mail@nonswag.dev>
2026-01-18 08:27:17 +0100

    Rename BukkitMetricsTest to MetricsTest

--- core/src/test/java/dev/faststats/MetricsTest.java
+++ core/src/test/java/dev/faststats/MetricsTest.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-public class BukkitMetricsTest {
+public class MetricsTest {
     @Test
     public void testCreateData() throws IOException {
         var mock = new MockMetrics(UUID.randomUUID(), "24f9fc423ed06194065a42d00995c600", true);
