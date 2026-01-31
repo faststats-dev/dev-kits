565f57b12016018f9275858bc719ef7df59d8a00
david <mail@nonswag.dev>
2026-01-31 16:05:04 +0100

    Simplify ServerPluginException handling logic

--- bukkit/src/main/java/dev/faststats/bukkit/PaperEventListener.java
+++ bukkit/src/main/java/dev/faststats/bukkit/PaperEventListener.java
@@ -2,7 +2,6 @@ package dev.faststats.bukkit;
 
 import com.destroystokyo.paper.event.server.ServerExceptionEvent;
 import com.destroystokyo.paper.exception.ServerPluginException;
-import dev.faststats.core.ErrorTracker;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,13 +9,8 @@ import org.bukkit.event.Listener;
 record PaperEventListener(BukkitMetricsImpl metrics) implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onServerException(final ServerExceptionEvent event) {
-        metrics.printError("Received exception", event.getException()); // todo: remove
-        if (event.getException() instanceof final ServerPluginException exception) {
-            if (!exception.getResponsiblePlugin().equals(metrics.plugin())) return;
-            metrics.getErrorTracker().ifPresent(tracker -> tracker.trackError(exception));
-        } else if (ErrorTracker.isSameLoader(metrics.plugin().getClass().getClassLoader(), event.getException())) {
-            metrics.printError("Received exception from same class loader", event.getException()); // todo: remove
-            metrics.getErrorTracker().ifPresent(tracker -> tracker.trackError(event.getException()));
-        }
+        if (!(event.getException() instanceof final ServerPluginException exception)) return;
+        if (!exception.getResponsiblePlugin().equals(metrics.plugin())) return;
+        metrics.getErrorTracker().ifPresent(tracker -> tracker.trackError(exception));
     }
 }
